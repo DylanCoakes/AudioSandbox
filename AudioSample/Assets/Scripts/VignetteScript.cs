7fd45e42aea7419fd963cd599f8001489ff39bd9
DylanCoakes <dylan.coakes@lethbridgecollege.ca>
2021-10-21 15:47:47 -0600

    Changed script name

--- AudioSample/Assets/Scripts/VignetteScript.cs
+++ AudioSample/Assets/Scripts/VignetteScript.cs
@@ -7,7 +7,7 @@ using UnityEngine.InputSystem;
 using System;
 
 public class VignetteScript : MonoBehaviour
-{
+{//intesity of the fade adn how long it lasts
     [SerializeField]
     float intensity = 0.75f;
     [SerializeField]
@@ -15,24 +15,26 @@ public class VignetteScript : MonoBehaviour
     [SerializeField]
     private Volume volume;
     Vignette vignette;
+    //can determine if the joystick is pressed and where that position is in game
     [SerializeField]
-    InputActionReference continousMove;
+    InputActionReference continuousMove;
 
+    //when joystick is pressed,fade in; when released, fade out.
     private void Awake()
     {
-        continousMove.action.performed += FadeIn;
-        continousMove.action.canceled += FadeOut;
+        continuousMove.action.performed += FadeIn;
+        continuousMove.action.canceled += FadeOut;
 
-        if (volume.profile.TryGet(out Vignette vignette))
-            this.vignette = vignette;
-
+       // if (volume.profile.TryGet(out Vignette vignette))
+         //   this.vignette = vignette;
     }
-
+     //when released fade out
     private void FadeOut(InputAction.CallbackContext obj)
     {
         StartCoroutine(Fade(0, intensity));
     }
-
+    
+    //when pressed, if value is not zero, fades in
     private void FadeIn(InputAction.CallbackContext obj)
     {
         if (obj.ReadValue<Vector2>() != Vector2.zero)
@@ -40,18 +42,18 @@ public class VignetteScript : MonoBehaviour
             StartCoroutine(Fade(intensity, 0));
         }
     }
-
     IEnumerator Fade(float startValue, float endValue)
     {
         float elapsedTime = 0.0f;
         float blend = elapsedTime / duration;
+        //calculating the intesisty of the vignette by / values
         float intensity = Mathf.Lerp(startValue, endValue, blend);
         ApplyValue(intensity);
         yield return null;
     }
-
+    //applying the inestity to the vignette
     void ApplyValue(float value)
     {
-        vignette.intensity.Override(value);
+        //vignette.intensity
     }
 }
