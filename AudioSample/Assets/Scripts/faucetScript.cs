1cdd709092c15f4be49e790716b1b284ea5973e3
DylanCoakes <dylan.coakes@lethbridgecollege.ca>
2021-10-22 13:06:32 -0600

    Commit, there are errors that will be fixed when script is complete. save and commit

--- AudioSample/Assets/Scripts/faucetScript.cs
+++ AudioSample/Assets/Scripts/faucetScript.cs
@@ -3,24 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class faucetScript : MonoBehaviour
+public class CoroutinesExample : MonoBehaviour
 {
-    public float waterFlow;
-    public Transform water;
-    public void StartWater()
-    {
-        // call ac coroutine to spawn droplets
-        StartCoroutine(MyCoroutine(water)); 
+    public float smoothing = 1f;
+    public Transform target;
 
-        IEnumerator MyCoroutine(Transform target)
+
+    void Start()
+    {
+        StartCoroutine(StartWater(target));
+    }
+
+
+    IEnumerator StartWater(Transform target)
+    {
+        while (Vector3.Distance(transform.position, target.position) > 0.05f)
         {
-            while (Vector3.Distance(transform.position, target.position) > 0.5f)
-            {
-                transform.position = Vector3.Lerp(transform.position, water.position, waterFlow * Time.deltaTime);
-            }
+            transform.position = Vector3.Lerp(transform.position, target.position, smoothing * Time.deltaTime);
 
             yield return null;
         }
+
+        print("Reached the target.");
+
+        yield return new WaitForSeconds(3f);
+
+        print("MyCoroutine is now finished.");
     }
 }
-
