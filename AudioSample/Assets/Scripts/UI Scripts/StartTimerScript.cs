aeb2de988f062edc0eb6409c5905614727c6f8b9
DylanCoakes <dylan.coakes@lethbridgecollege.ca>
2021-11-24 14:09:02 -0700

    XR rig does not work so i cannot test the game. Create a xr rig that works.

--- AudioSample/Assets/Scripts/UI Scripts/StartTimerScript.cs
+++ AudioSample/Assets/Scripts/UI Scripts/StartTimerScript.cs
@@ -11,20 +11,39 @@ public class StartTimerScript : MonoBehaviour
     //max time counting down from
     private float mainTimer;
 
+
     private float timer;
-    private bool canCOunt = true;
+    private bool canCount = true;
     private bool doOnce = false;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private void Start()
+    {//the timer is what we set it in the inspector
+        timer = mainTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-       if (timer ?=0.0f ) 
+       if (timer >=0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            uiText.text = timer.ToString("f");
+
+        }
+       else if (timer <= 0.0 && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            uiText.text = "0.00";
+            timer = 0.00f;
+        }
+    }
+    //once the button is pressed, reset the timer.
+    public void resetButton()
+    {
+        timer = mainTimer;
+        canCount = true;
+        doOnce = false;
     }
 }
+
